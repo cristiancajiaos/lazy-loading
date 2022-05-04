ddbf8492751a4da1d091cd1b5814e51dd264d3d2
Cristian Cajiao <cristiancajiaos@outlook.com>
2022-05-04 10:58:22 -0400

    Creados módulos base y ruteos básicos a ellos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: 'customers', loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule) },
+  { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
